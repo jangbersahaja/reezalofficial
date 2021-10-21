9fbac73d262829d4324655afce822a6c96dc519b
Hasrul Fahmizan Hashim <hasrulfahmizanhashim@Jangs-Mac-Air.local>
2021-10-21 10:11:44 +0800

    Updates

---
+++ precache-manifest.f567b44330f7cdf30bcc04382cee22c1.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52d4e1d94e3fda5d8464939f76c06b12",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b738098321ee3444a839",
+    "url": "/static/js/2.2691066b.chunk.js"
+  },
+  {
+    "revision": "bd96a1cfc47c001e68467155960da745",
+    "url": "/static/js/2.2691066b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f27e18632c2474c5c5e",
+    "url": "/static/js/main.dcf243d3.chunk.js"
+  },
+  {
+    "revision": "db3b8a25ec23076e2ae2",
+    "url": "/static/js/runtime-main.2a506d3f.js"
+  },
+  {
+    "revision": "c056469c44d7cb8b89a7017a69922657",
+    "url": "/static/media/about1.c056469c.jpg"
+  },
+  {
+    "revision": "c1705eb60dfe16bfe1b16b2eb45706ca",
+    "url": "/static/media/about2.c1705eb6.jpg"
+  },
+  {
+    "revision": "61486d07ee74b64f1a261f8b432063e0",
+    "url": "/static/media/about3.61486d07.jpg"
+  },
+  {
+    "revision": "a9394f813c1c99bcfc1d0a1bb28cf780",
+    "url": "/static/media/article-pm.a9394f81.jpg"
+  },
+  {
+    "revision": "5d03884fdab63defb9c30cac2933881a",
+    "url": "/static/media/article1.5d03884f.jpg"
+  },
+  {
+    "revision": "23b0aa6858b96222c4c73784b47af2b9",
+    "url": "/static/media/article3.23b0aa68.jpg"
+  },
+  {
+    "revision": "2678ba49cc605d5950cc56b132e102b7",
+    "url": "/static/media/article4.2678ba49.jpg"
+  },
+  {
+    "revision": "466a3ca5bbb33d7b28b1ca2827ce8b0e",
+    "url": "/static/media/blog-hero.466a3ca5.jpg"
+  },
+  {
+    "revision": "44660871247b261a84fb5cd79dc7c282",
+    "url": "/static/media/hero-bg.44660871.jpg"
+  },
+  {
+    "revision": "15cc3b73632b05d7d69cbcc5f58ca5ca",
+    "url": "/static/media/hundredday-bg.15cc3b73.jpg"
+  },
+  {
+    "revision": "7f96d7685d5355bbd2138ca29c2ecd6e",
+    "url": "/static/media/kepalabatas-bg.7f96d768.jpg"
+  },
+  {
+    "revision": "bcdfffa31ed680fc51bff04092fa2b61",
+    "url": "/static/media/lembahsubang1.bcdfffa3.jpg"
+  },
+  {
+    "revision": "aa9bd679113772c5282f5b500818a3d7",
+    "url": "/static/media/lembahsubang2.aa9bd679.jpg"
+  },
+  {
+    "revision": "1f477b32db6ed3fa8a4c19c31d421a9b",
+    "url": "/static/media/lembahsubang3.1f477b32.jpg"
+  },
+  {
+    "revision": "b1a10ffff08d81f92a34d3b2969be0a3",
+    "url": "/static/media/lembahsubang4.b1a10fff.jpg"
+  },
+  {
+    "revision": "872f3878527eec0e7e8cce80fe26f8cc",
+    "url": "/static/media/liveable1.872f3878.jpg"
+  },
+  {
+    "revision": "f59b4e3893e7288be897cc6050601659",
+    "url": "/static/media/liveable2.f59b4e38.jpg"
+  },
+  {
+    "revision": "42d051e713ca700aba0346b6fc6c7284",
+    "url": "/static/media/liveable3.42d051e7.jpg"
+  },
+  {
+    "revision": "c2988f2ede8fd58ff2452092ff037e1a",
+    "url": "/static/media/logo-ikram.c2988f2e.png"
+  },
+  {
+    "revision": "8860ce801a9e54f820c986c23baba1e0",
+    "url": "/static/media/logo.8860ce80.png"
+  },
+  {
+    "revision": "3166e7073a8b26bd8a9634457a6a7c8e",
+    "url": "/static/media/ppr.3166e707.jpg"
+  },
+  {
+    "revision": "53e6ef2e3227ecccd0233bc189fb47dd",
+    "url": "/static/media/profile.53e6ef2e.png"
+  },
+  {
+    "revision": "cc0a9810774f9567a7597d29842be252",
+    "url": "/static/media/rumah-ikram.cc0a9810.jpg"
+  },
+  {
+    "revision": "dbf5d547f25be951d7b0d108e1f20ae4",
+    "url": "/static/media/rumah-ikram2.dbf5d547.jpg"
+  },
+  {
+    "revision": "b0bcace8387859017ebe74077d45fa95",
+    "url": "/static/media/rumah-ikram3.b0bcace8.jpg"
+  },
+  {
+    "revision": "0c6b1c000704c8d52432ffdb42613ead",
+    "url": "/static/media/rumah-ikram4.0c6b1c00.jpg"
+  }
+]);
