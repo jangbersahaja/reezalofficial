ed2b9930f2d8cd330cfc64b6cf432d4624e9580f
Hasrul Fahmizan Hashim <hasrulfahmizanhashim@Jangs-Mac-Air.local>
2021-10-26 21:26:38 +0800

    fix about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,8 @@
-import React, { useState } from "react";
-
+import React from "react";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import styled from "styled-components";
 import ProfilePhoto from "../img/profile.png";
-import { Document, Page, pdfjs } from "react-pdf";
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const Container = styled.div`
   display: flex;
@@ -88,7 +85,7 @@ const Title = styled.h2`
   color: #37517e;
   text-align: center;
   border-bottom: 7px solid #d43076;
-
+  
   @media (max-width: 768px) {
     font-size: 48px;
   }
@@ -99,35 +96,41 @@ const Text = styled.p`
   text-align: justify;
 `;
 
-const About = () => {
-  const [numPages, setNumPages] = useState(null);
-  const [pageNumber, setPageNumber] = useState(1);
-
-  const onDocumentLoadSuccess = ({ numPages }) => {
-    setNumPages(numPages);
-  };
-
+function About() {
   return (
     <div>
       <Navbar />
       <Container>
         <Wrapper>
-          <div>
-            <Document
-              file="https://www.kpkt.gov.my/kpkt/resources/user_1/media_akhbar/2021/SM_PENJELASAN_KPKT_HBA_BINGUNG_JUMLAH_PROJEK_RUMAH_TERBENGKALAI_TAK_SAMA.pdf"
-              onLoadSuccess={onDocumentLoadSuccess}
-            >
-              <Page pageNumber={pageNumber} />
-            </Document>
-            <p>
-              Page {pageNumber} of {numPages}
-            </p>
-          </div>
+          <ImgWrapper>
+            <Image />
+          </ImgWrapper>
+          <TextWrapper>
+            <Title>About <br/> Reezal Merican</Title>
+            <Text>
+              Politician, Member of Parliament for Kepala Batas, and serves the
+              Malaysian Government. Reezal Merican Naina Merican is the current
+              Minister of Housing and Local Government.
+              {"\n"} {"\n"}
+              During his tenure as the Minister of Youth and Sports, Reezal has
+              devoted his career to focus on strengthening Youth and Malaysian
+              Athletes. He has led the Malaysian Contingent during the Tokyo
+              2020 Olympics where the athletes successfully brought home one
+              silver and one bronze medal.
+              {"\n"} {"\n"}
+              He is involved in various initiatives under both state and
+              national programmes advocating for the long-term benefit of
+              Malaysian society and the world community.
+              {"\n"} {"\n"}
+              His past experience also includes a strong portfolio serving in
+              both policy-making and foreign affairs throughout his career.
+            </Text>
+          </TextWrapper>
         </Wrapper>
       </Container>
       <Footer />
     </div>
   );
-};
+}
 
 export default About;
